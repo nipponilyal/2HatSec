30b57cbfc39959333c5f620ce77a677f39f02bd0
Ilya Levin <ilevin@lixar.com>
2019-09-12 02:02:16 -0300

    readme

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ set -e
 
 docker build . -t handler
 kubectl apply -f deployment/handler.yaml
-sleep 60
-kubectl delete -f deployment/handler.yaml
+# sleep 60
+# kubectl delete -f deployment/handler.yaml
